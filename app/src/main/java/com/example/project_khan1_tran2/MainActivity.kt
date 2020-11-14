7dc8779d3a34463d6e70f5d414ed058bc4aae1c5
sahil2016sam <ammarummar@hotmail.com>
2020-11-14 14:33:49 -0800

    test run

---
+++ app/src/main/java/com/example/project_khan1_tran2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project_khan1_tran2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //this is a test
+    }
+}
