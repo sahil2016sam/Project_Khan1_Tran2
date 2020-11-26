5b2819f4c678060cf16135360f768d66f7842d22
Kevin Tran <bleemaster88@gmail.com>
2020-11-26 15:45:40 -0500

    Added Text views, radio button, and button for Login Page. Added strings to strings.xml. Added Text views, and buttons to modify_record_activity.xml

--- app/src/main/java/com/example/project_khan1_tran2/LoginScreenActivity.kt
+++ app/src/main/java/com/example/project_khan1_tran2/LoginScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.example.project_khan1_tran2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,12 @@ class LoginScreenActivity : AppCompatActivity() {
         // test run 3
         // stop with this madness!
         ///ajaj
+
+    }
+
+    fun loginClick(view: View)
+    {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
     }
 }
