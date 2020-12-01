f10ac937800b26f48383c0b5fd9dae85f9009354
Kevin Tran <bleemaster88@gmail.com>
2020-11-30 20:03:04 -0500

    Added on click function with url, dependencies etc...

--- app/src/main/java/com/example/project_khan1_tran2/LoginScreenActivity.kt
+++ app/src/main/java/com/example/project_khan1_tran2/LoginScreenActivity.kt
@@ -1,7 +1,10 @@
 package com.example.project_khan1_tran2
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.login_screen_activity.*
 
 class LoginScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,11 @@ class LoginScreenActivity : AppCompatActivity() {
         // test run 3
         // stop with this madness!
         ///ajaj
+    }
+
+    fun loginClick(view: View) {
+        val url =
+                "http://mohameom.dev.fast.sheridanc.on.ca/users/verifyUserData.php?name=${userNameEDT.text}&password=${passwordEDT.text}"
+
     }
 }
