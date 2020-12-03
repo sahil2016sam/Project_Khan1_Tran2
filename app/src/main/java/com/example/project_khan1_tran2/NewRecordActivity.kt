6e6be6c37062e2f82b177411e2fe6796674ae69c
sahil2016sam <ammarummar@hotmail.com>
2020-12-02 17:50:02 -0800

    still setting up Room database with recycler view

--- app/src/main/java/com/example/project_khan1_tran2/NewRecordActivity.kt
+++ app/src/main/java/com/example/project_khan1_tran2/NewRecordActivity.kt
@@ -1,11 +1,13 @@
 package com.example.project_khan1_tran2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import com.example.project_khan1_tran2.roomdatabase.MyViewModel
 import kotlinx.android.synthetic.main.all_records_activity.*
+import kotlinx.android.synthetic.main.new_record_activity.*
 
 class NewRecordActivity : AppCompatActivity() {
 
@@ -15,6 +17,11 @@ class NewRecordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_record_activity)
         //implementation for room database
+
+        saveRecordBTN.setOnClickListener{
+            val intent = Intent(this, AllRecordsActivity::class.java)
+            startActivity(intent)
+        }
 
         //creating an instance for ViewModelProvider
         vm = ViewModelProvider(this, ViewModelProvider
