eb081dee7c27b360612bbc68832b223f8da31f23
Kevin Tran <bleemaster88@gmail.com>
2020-12-08 18:36:44 -0500

    Added networking, If statement in login screen doesn't work yet (placed true statement there for now).

--- app/src/main/java/com/example/project_khan1_tran2/NetworkingViewModel.kt
+++ app/src/main/java/com/example/project_khan1_tran2/NetworkingViewModel.kt
@@ -2,7 +2,14 @@ package com.example.project_khan1_tran2
 
 import android.app.Application
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.project_khan1_tran2.roomdatabase.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.BufferedReader
@@ -14,9 +21,13 @@ import java.net.URL
 
 class NetworkingViewModel(app: Application) : AndroidViewModel(app) {
 
-    fun loadData(urlString: String){
+    var username = MutableLiveData<String>()
+    var user = MutableLiveData<User>()
+    var result = ""
+
+    suspend fun loadData(urlString: String):String{
         var ins : InputStream? = null
-        var result = ""
+         result = ""
         try {
         var url = URL(urlString)
             val conn =url.openConnection() as HttpURLConnection
@@ -26,17 +37,35 @@ class NetworkingViewModel(app: Application) : AndroidViewModel(app) {
             ins = conn.inputStream
 
             result = ins.bufferedReader().use(BufferedReader::readText)
+            var json = JSONObject(result)
+            var name = json.optString("name")
+            var password = json.optString("password")
             Log.d("result", result)
         }catch (e: Exception){
             Log.d("TAG", e.toString())
         }
+        return result
     }
     fun makeRequest(url:String){
+        CoroutineScope(Dispatchers.IO).launch {
+             result = loadData(url)
+
+            withContext(Dispatchers.Main){
+               var json = getJson(result)
+                var name = json.optString("name")
+                var password = json.optString("password")
+                this@NetworkingViewModel.username.value = name
+                this@NetworkingViewModel.user.value = User(name, password)
+                Toast.makeText(getApplication(), name, Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 
-    fun getJson(jsonStr : String) : JSONObject {
+    fun getJson(jsonStr : String):JSONObject  {
         var json  = JSONObject(jsonStr)
+        Log.d("TAG", jsonStr)
         return json
+
     }
 }
---
+++ app/src/main/java/com/example/project_khan1_tran2/roomdatabase/User.kt
@@ -0,0 +1,9 @@
+package com.example.project_khan1_tran2.roomdatabase
+
+data class User (
+        var name:String,
+        var password:String
+)
+{
+
+}
